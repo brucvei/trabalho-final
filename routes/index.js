b307585738afc499c64efd04d676093bbb366e23
Bruna Caetano <bruna.caetano@acad.ufsm.br>
2023-11-13 15:36:49 -0300

    falta mt coisa mds

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,23 @@
 var express = require('express');
+const axios = require("axios");
 var router = express.Router();
+const url = "http://localhost:8080/";
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Tratamento de JSON com JAVA' });
+router.get('/', function (req, res, next) {
+  axios.get(url).then(response => {
+    var options = {
+      title: response.data,
+      notas: null
+    };
+    axios.get(url + "nota/all").then(notas => {
+      options.notas = notas.data;
+      res.render('index', options);
+      console.log(notas.data);
+    });
+  }).catch(error => {
+    console.log(error);
+  });
 });
 
 module.exports = router;
