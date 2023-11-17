daae6d62687efd29f9e33ac8de093e3d3226df7e
Bruna Caetano <bruna.caetano@acad.ufsm.br>
2023-11-17 14:54:05 -0300

    com certeza kkkkkkkk

--- routes/index.js
+++ routes/index.js
@@ -1,22 +1,52 @@
 var express = require('express');
 const axios = require("axios");
 var router = express.Router();
-const url = "http://localhost:8080/";
+const url = "http://localhost:8080";
+const options = {
+  title: "",
+  notas: null,
+  impostos: {
+    issqn: null,
+    icms: {
+      COFINS: null,
+      BCST: null,
+      Prod: null,
+      Seg: null,
+      NF: null,
+      PIS: null,
+      BC: null,
+      ST: null,
+      ICMS: null,
+      II: null,
+      Desc: null,
+      Outro: null,
+      IPI: null,
+      Frete: null,
+    },
+    tributos: null
+  },
+};
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
   axios.get(url).then(response => {
-    var options = {
-      title: response.data,
-      notas: null
-    };
-    axios.get(url + "nota/all").then(notas => {
+    options.title = response.data;
+    axios.get(url + "/nota/all").then(notas => {
       options.notas = notas.data;
-      res.render('index', options);
-      console.log(notas.data);
+      axios.get(url + "/issqn").then(issqn => {
+        options.impostos.issqn = issqn.data;
+        axios.get(url + "/icms").then(icms => {
+          options.impostos.icms = icms.data;
+          axios.get(url + "/tributos").then(tributos => {
+            options.impostos.tributos = tributos.data;
+            res.render('index', options);
+          });
+        });
+      });
     });
   }).catch(error => {
     console.log(error);
+    res.render('index', { title: 'Error' });
   });
 });
 
