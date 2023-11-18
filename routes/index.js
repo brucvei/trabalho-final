9c40cc1f0c09bb5921f5a8f8b562ef6604aa75db
Bruna Caetano <bruna.caetano@acad.ufsm.br>
2023-11-18 12:50:24 -0300

    wip

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ var router = express.Router();
 const url = "http://localhost:8080";
 const options = {
   title: "",
+  qntdNotas: null,
   notas: null,
   impostos: {
     issqn: null,
@@ -25,28 +26,64 @@ const options = {
     },
     tributos: null
   },
+  valorProdutos: null,
+  valorFrete: null,
+  qntdProdutos: null,
+  produtos: [{
+    nNf: null,
+    cProd: null,
+    cEAN: null,
+    xProd: null,
+    NCM: null,
+    CFOP: null,
+    uCom: null,
+    qCom: null,
+    vUnCom: null,
+    vProd: null,
+    cEANTrib: null,
+    uTrib: null,
+    qTrib: null,
+    vUnTrib: null,
+    indTot: null
+  }],
 };
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
   axios.get(url).then(response => {
     options.title = response.data;
-    axios.get(url + "/nota/all").then(notas => {
-      options.notas = notas.data;
-      axios.get(url + "/issqn").then(issqn => {
-        options.impostos.issqn = issqn.data;
-        axios.get(url + "/icms").then(icms => {
-          options.impostos.icms = icms.data;
-          axios.get(url + "/tributos").then(tributos => {
-            options.impostos.tributos = tributos.data;
-            res.render('index', options);
+    axios.get(url + "/nota/quantidade").then(notas => {
+      options.qntdNotas = notas.data;
+      axios.get(url + "/nota/all").then(notas => {
+        options.notas = notas.data;
+        axios.get(url + "/issqn").then(issqn => {
+          options.impostos.issqn = issqn.data;
+          axios.get(url + "/icms").then(icms => {
+            options.impostos.icms = icms.data;
+            axios.get(url + "/tributos").then(tributos => {
+              options.impostos.tributos = tributos.data;
+              axios.get(url + "/produtos/valor-total").then(valorProdutos => {
+                options.valorProdutos = valorProdutos.data;
+                axios.get(url + "/frete").then(valorFrete => {
+                  options.valorFrete = valorFrete.data;
+                  axios.get(url + "/produtos/quantidade").then(qntdProdutos => {
+                    options.qntdProdutos = qntdProdutos.data;
+                    axios.get(url + "/produtos").then(produtos => {
+                      options.produtos = produtos.data;
+                      console.log(options.produtos)
+                      res.render('index', options);
+                    });
+                  });
+                });
+              });
+            });
           });
         });
       });
     });
   }).catch(error => {
     console.log(error);
-    res.render('index', { title: 'Error' });
+    res.render('index', {title: 'Error'});
   });
 });
 
